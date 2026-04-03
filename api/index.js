ae9d026be73e30f0764805056831c1dfa24a0815
berndrollerconcerts-commits <berndrollerconcerts@gmail.com>
2026-04-03 17:21:45 +0200

    index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,25 @@
+const axios = require("axios");
 
+module.exports = async (req, res) => {
+  try {
+    const authString = Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString("base64");
+    const tokenRes = await axios.post("https://accounts.spotify.com/api/token", "grant_type=client_credentials", {
+      headers: { Authorization: `Basic ${authString}`, "Content-Type": "application/x-www-form-urlencoded" }
+    });
+    
+    const token = tokenRes.data.access_token;
+    const albumId = "0kQNJeVJkWP3ViM4UokDAS"; // Kiss me in Palermo
+
+    const response = await axios.get(`https://api.spotify.com/v1/albums/${albumId}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+
+    res.status(200).json({
+      name: response.data.name,
+      cover: response.data.images[0]?.url,
+      link: response.data.external_urls.spotify
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Spotify Fehler", details: err.message });
+  }
+};
