92d89eaaf085b8b45c1428a3dfa557dca04157c8
berndrollerconcerts-commits <berndrollerconcerts@gmail.com>
2026-04-03 16:44:20 +0200

    index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ const axios = require("axios");
 
 module.exports = async (req, res) => {
   try {
-    // 1. Token holen
     const authString = Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString("base64");
+    
+    // 1. Token holen
     const tokenResponse = await axios.post("https://accounts.spotify.com/api/token", "grant_type=client_credentials", {
       headers: { 
         Authorization: `Basic ${authString}`, 
@@ -12,20 +13,20 @@ module.exports = async (req, res) => {
     });
     
     const token = tokenResponse.data.access_token;
-    const albumId = "0kQNJeVJkWP3ViM4UokDAS"; // Kiss me in Palermo
+    const albumId = "0kQNJeVJkWP3ViM4UokDAS"; 
 
-    // 2. Album Daten holen
+    // 2. Daten von Spotify abrufen
     const response = await axios.get(`https://api.spotify.com/v1/albums/${albumId}`, {
       headers: { Authorization: `Bearer ${token}` }
     });
 
-    // 3. Antwort senden
+    // 3. Antwort an deine Website senden
     res.status(200).json({
       name: response.data.name,
       cover: response.data.images[0]?.url,
       link: response.data.external_urls.spotify
     });
   } catch (err) {
-    res.status(500).json({ error: "Spotify Fehler", msg: err.message });
+    res.status(500).json({ error: "Spotify Fehler", details: err.message });
   }
 };
